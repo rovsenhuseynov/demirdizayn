c3a8bf4a4c89d49fc9a88f1031b53c78eef94960
rovshen <rovsen.dev@gmail.com>
2025-07-14 21:04:19 +0400

    Сборка header backgrounds

--- src/globalComponents/Header/Header.jsx
+++ src/globalComponents/Header/Header.jsx
@@ -1,3 +1,26 @@
+// import "./Header.scss";
+// import HeaderNav from "./HeaderNav/HeaderNav";
+
+// const Header = () => {
+//   return (
+//     <div className="header">
+//       <HeaderNav />
+//       <div className="header__title">
+//         <h1 className="header__main-title">
+//           Loft Mebel və
+//           <br />
+//           <span className="highlight">Metal İşləri</span>
+//         </h1>
+//         <p className="header__subtitle">Качество на высоком уровне</p>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Header;
+
+
+
 
 import "./Header.scss";
 import HeaderNav from "./HeaderNav/HeaderNav";
@@ -6,6 +29,14 @@ const Header = () => {
   return (
     <div className="header">
       <HeaderNav />
+      <div className="header__title">
+        <h1 className="header__main-title">
+          Loft Mebel və
+          <br />
+          Metal İşləri
+        </h1>
+        <p className="header__subtitle">Качество на высоком уровне</p>
+      </div>
     </div>
   );
 };
