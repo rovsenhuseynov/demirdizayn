23c1344a34186bee67b0554057d546b3d95eb25c
rovshen <rovsen.dev@gmail.com>
2025-07-26 22:32:24 +0400

    Работа над сомпонентом Footer

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Contacts from "./pages/Contacts/Contacts";
 import Jurnal from "./pages/Jurnal/Jurnal";
 import Products from "./pages/Products/Products";
 import Services from "./pages/Services/Services";
+import AboutUs from "./pages/AboutUs/AboutUs";
 
 
 const App = () => {
@@ -17,6 +18,7 @@ const App = () => {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="/services" element={<Services />} />
+          <Route path="/about" element={<AboutUs />} />
           <Route path="/products" element={<Products />} />
           <Route path="/faq" element={<Faq />} />
           <Route path="/jurnal" element={<Jurnal />} />
--- src/globalComponents/Footer/Footer.jsx
+++ src/globalComponents/Footer/Footer.jsx
@@ -1,9 +1,75 @@
+import Logo from "../../reusableСomponents/Logo/Logo";
+import instagramIcon from "../../assets/icons/Social_networks/instagram/instagram.svg";
+import facebookIcon from "../../assets/icons/Social_networks/facebook/facebook.svg";
+import telegramIcon from "../../assets/icons/Social_networks/telegram/telegram.svg";
+import whatsappIcon from "../../assets/icons/Social_networks/whatsapp/whatsapp.svg";
 import "./Footer.scss";
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+  const currentYear = new Date().getFullYear();
 
-export default Footer
+  return (
+    <footer className="footer">
+      {/* Верхний блок: логотип, меню и соцсети */}
+      <div className="footer__main">
+        <div className="footer__container">
+          <div className="footer__logo">
+            <Logo />
+          </div>
+
+          <nav className="footer__nav">
+            <ul className="footer__menu">
+              <li>
+                <a href="/about">About Us</a>
+              </li>
+              <li>
+                <a href="/products">Catalog</a>
+              </li>
+              <li>
+                <a href="/contacts">Contacts</a>
+              </li>
+            </ul>
+          </nav>
+
+          <div className="footer__socials">
+            <a href="https://instagram.com" target="_blank" rel="noreferrer">
+              <img src={instagramIcon} alt="Instagram" />
+            </a>
+            <a href="https://facebook.com" target="_blank" rel="noreferrer">
+              <img src={facebookIcon} alt="Facebook" />
+            </a>
+            <a href="https://telegram.org" target="_blank" rel="noreferrer">
+              <img src={telegramIcon} alt="Telegram" />
+            </a>
+            <a href="https://whatsapp.com" target="_blank" rel="noreferrer">
+              <img src={whatsappIcon} alt="WhatsApp" />
+            </a>
+          </div>
+        </div>
+      </div>
+
+      {/* Нижний блок: копирайт */}
+      <div className="footer__bottom">
+        <div className="footer__container">
+          <p>
+            © {currentYear} LoftCraft. All rights reserved. |{" "}
+            <a
+              href="/privacy-policy"
+              style={{ color: "#bbb", textDecoration: "underline" }}
+            >
+              Privacy Policy
+            </a>
+            <a
+              href="/terms-of-service"
+              style={{ color: "#bbb", textDecoration: "underline" }}
+            >
+              Terms of Service
+            </a>
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
