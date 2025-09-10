f948adeab387085c9db5b2f5f82a904ab5e20543
rovshen <rovsen.dev@gmail.com>
2025-09-10 13:24:03 +0400

    Коррекция компонента - PhotoGallery

--- src/reusableComponents/PhotoGallery/PhotoGallery.jsx
+++ src/reusableComponents/PhotoGallery/PhotoGallery.jsx
@@ -1,71 +1,3 @@
-// import { useState } from "react";
-// import ReactDOM from "react-dom";
-// import "./PhotoGallery.scss";
-
-// const Modal = ({ closeModal, src }) => {
-//   return (
-//     <div className="modal-overlay" onClick={closeModal}>
-//       <div className="modal">
-//         <img src={src} alt="Modal" />
-//       </div>
-//     </div>
-//   );
-// };
-
-// const PhotoGallery = ({ images1 = [], images2 = [] }) => {
-//   const [modalOpen, setModalOpen] = useState(false);
-//   const [modalImageSrc, setModalImageSrc] = useState("");
-
-//   const openModal = (src) => {
-//     setModalImageSrc(src);
-//     setModalOpen(true);
-//   };
-
-//   const closeModal = () => {
-//     setModalOpen(false);
-//   };
-
-//   return (
-//     <div className="photo-gallery__wrapper">
-//       <div id="grid-container">
-//         <div className="grid">
-//           <div className="row forward">
-//             {images1.map((src, index) => (
-//               <div
-//                 key={index}
-//                 className={`box fill${(index % 4) + 1}`}
-//                 onClick={() => openModal(src)}
-//               ></div>
-//             ))}
-//           </div>
-
-//           <div className="row backward">
-//             {images2.map((src, index) => (
-//               <div
-//                 key={index}
-//                 className={`box fill${(index % 4) + 5}`}
-//                 onClick={() => openModal(src)}
-//               ></div>
-//             ))}
-//           </div>
-//         </div>
-//       </div>
-
-//       {modalOpen &&
-//         ReactDOM.createPortal(
-//           <Modal closeModal={closeModal} src={modalImageSrc} />,
-//           document.getElementById("modal-root")
-//         )}
-//     </div>
-//   );
-// };
-
-// export default PhotoGallery;
-
-
-
-
-
 import { useState } from "react";
 import ReactDOM from "react-dom";
 import "./PhotoGallery.scss";
@@ -94,38 +26,41 @@ const PhotoGallery = ({ images1 = [], images2 = [] }) => {
   };
 
   return (
-    <div className="photo-gallery__wrapper">
-      <div id="grid-container">
-        <div className="grid">
-          <div className="row forward">
-            {images1.map((src, index) => (
-              <div
-                key={index}
-                className="box"
-                style={{ backgroundImage: `url(${src})` }}
-                onClick={() => openModal(src)}
-              ></div>
-            ))}
-          </div>
+    <div className="photo-gallery__outer">
+      {/* <h2>Here may be the block title</h2> */}
+      <div className="photo-gallery__wrapper">
+        <div id="grid-container">
+          <div className="grid">
+            <div className="row forward">
+              {images1.map((src, index) => (
+                <div
+                  key={index}
+                  className="box"
+                  style={{ backgroundImage: `url(${src})` }}
+                  onClick={() => openModal(src)}
+                ></div>
+              ))}
+            </div>
 
-          <div className="row backward">
-            {images2.map((src, index) => (
-              <div
-                key={index}
-                className="box"
-                style={{ backgroundImage: `url(${src})` }}
-                onClick={() => openModal(src)}
-              ></div>
-            ))}
+            <div className="row backward">
+              {images2.map((src, index) => (
+                <div
+                  key={index}
+                  className="box"
+                  style={{ backgroundImage: `url(${src})` }}
+                  onClick={() => openModal(src)}
+                ></div>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
 
-      {modalOpen &&
-        ReactDOM.createPortal(
-          <Modal closeModal={closeModal} src={modalImageSrc} />,
-          document.getElementById("modal-root")
-        )}
+        {modalOpen &&
+          ReactDOM.createPortal(
+            <Modal closeModal={closeModal} src={modalImageSrc} />,
+            document.getElementById("modal-root")
+          )}
+      </div>
     </div>
   );
 };
